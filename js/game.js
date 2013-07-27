13abbc8c9ef382ed3ec04da1327e581d9126294b
Keva161 <keva161@Kevins-MacBook-Pro.local>
2013-07-27 08:13:50 +0100

    Added basic functionatly

--- js/game.js
+++ js/game.js
@@ -1,12 +1,19 @@
 function init() {
 
 	// Grab the 'game' element and store it in the variable 'canvas'
-	var canvas = document.getElementById('game');
+
+var canvas = document.getElementById('game');
+
+var ctx = canvas.getContext('2d');
+
+var button = document.getElementById('button')
+
+
 
 	// set the context of the canvas to 2d and store it in the variable ctx
-	var ctx = canvas.getContext('2d');
 
-	// Fill the screen with a black background
+
+	// Fill the screen with a white background
 	ctx.fillStyle = '#ffffff';
 	ctx.fillRect (0, 0, canvas.width, canvas.height);
 	
@@ -30,5 +37,14 @@ function init() {
 	//ctx.fillStyle = '#000000';
 	//ctx.fillText (phrase, xcoord, 200);
 	//ctx.textAlign = 'center';
+	
+	button.addEventListener('click', function() {
+		ctx.beginPath();
+        ctx.moveTo(x1.value, y1.value);
+        ctx.lineTo(x2.value, y2.value);
+        ctx.stroke();}, false);
+        };
 
-}
+function clickHandler() {
+	ctx.clearRect (0, 0, canvas.width, canvas.height);
+};
